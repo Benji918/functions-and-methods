0ebda9613b05d848f300b777b60b2e5bca24f529
Benjamin <kodiugos@gmail.com>
2024-08-27 21:46:20 -0500

    Add project files.

---
+++ functions and methods/Program.cs
@@ -0,0 +1,20 @@
+﻿//declaring a method
+void WritingSomething()
+{
+    Console.WriteLine("I'm writing something");
+}
+Console.WriteLine("This is outside the method");
+
+//calling method
+WritingSomething();
+
+//A method that has a parameter string mystring
+void WriteSomethingSpecific(string mystring)
+{
+    Console.WriteLine(mystring);
+}
+
+
+WriteSomethingSpecific("benjamin");
+
+Console.ReadKey();
